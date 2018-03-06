a233a45698ff01021dde7b504ca3c281a41d618b
Jan Cislinsky <jan.cislinsky@etnetera.cz>
2018-03-06 12:05:28 +0100

    🔖 Releases 2.0

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "ETLiveData",
+    name: "ETBinding",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "ETLiveData",
-            targets: ["ETLiveData"]),
+            name: "ETBinding",
+            targets: ["ETBinding"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ETLiveData",
+            name: "ETBinding",
             dependencies: []),
         .testTarget(
-            name: "ETLiveDataTests",
-            dependencies: ["ETLiveData"]),
+            name: "ETBindingTests",
+            dependencies: ["ETBinding"]),
     ]
 )
